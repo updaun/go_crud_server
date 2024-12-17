da97f2cbf049690bcb3e779f65727976b666c538
updaun <updauney@daum.net>
2024-12-17 12:59:50 +0900

    chore: helloworld

--- init/main.go
+++ init/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", helloworld)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Failed to start server", err)
+		panic(err)
+	}
+}
+
+func helloworld(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hello world")
 }
