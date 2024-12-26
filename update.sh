e331fe2f3469b0fe8d23aed98942fe54963ac0fe
updaun <updauney@daum.net>
2024-12-26 18:53:00 +0900

    deploy: add scripts

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+INIT_PATH="init"
+BUILD_NAME="server"
+BRANCH="main"
+
+PID=$(sudo lsof -i | grep $BUILD_NAME | awk '{print $2}')
+
+echo $PID
+
+kill -9 $PID
+
+git checkout $BRANCH
+git pull origin $BRANCH
+
+cd $INIT_PATH
+go build -o $BUILD_NAME
+
+echo $PWD
+
+nohup ./$BUILD_NAME &
